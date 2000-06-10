e1e28d5428e24cabd93aacba5e904f187fbaa057
starlab <starlab>
2000-06-10 01:36:50 +0000

    Initial revision

---
+++ sbin/earliest_time.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/*  Print out the (time_t) mtime of the least recently modified file
+ *  specified on the command line.
+ */
+
+main(int argc, char *argv[])
+{
+    int i;
+    time_t latest_time = -1;
+
+    for (i = 1; i < argc; i++) {
+        struct stat buffer;
+	if (stat(argv[i], &buffer) == 0) {
+	  if (latest_time < 0 || buffer.st_mtime < latest_time)
+	    latest_time = buffer.st_mtime;
+	}
+    }
+    printf("%d\n", latest_time);
+}
