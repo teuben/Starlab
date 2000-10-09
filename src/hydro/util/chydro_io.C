cc0f99edea527f0e445aa9a5a83f258006e6c1f6
starlab <starlab>
2000-10-09 23:57:53 +0000

    Initial revision

---
+++ src/hydro/util/chydro_io.C
@@ -0,0 +1,59 @@
+//
+// chydro_io.C
+//
+
+#include "chydro.h"
+#include "util_io.h"
+
+istream & chydro::scan_hydro_story(istream& s)
+{
+    char input_line[MAX_INPUT_LINE_LENGTH];
+
+    while(get_line(s,input_line), strcmp(END_HYDRO, input_line)){
+	char keyword[MAX_INPUT_LINE_LENGTH];
+	char should_be_equal_sign[MAX_INPUT_LINE_LENGTH];
+	sscanf(input_line,"%s%s",keyword,should_be_equal_sign);
+	if(strcmp("=",should_be_equal_sign)){
+	    cerr << "Expected '=', but got '"<< should_be_equal_sign <<"'\n";
+	    exit(1);
+	}
+
+    	if(0){   // trick to keep the if() statement, for the else below
+	}else if(!strcmp("R_eff",keyword)){
+	    sscanf(input_line,"%*s%*s%lf",&effective_radius);
+    	}else if(!strcmp("M_core",keyword)){
+	    sscanf(input_line,"%*s%*s%lf",&core_mass);
+	}else if(!strcmp("R_core",keyword)){
+	    sscanf(input_line,"%*s%*s%lf",&core_radius);
+	}else if(!strcmp("mf",keyword)){
+            sscanf(input_line,"%*s%*s%lf",&m_conv_hydro_to_dyn);
+	}else if(!strcmp("rf",keyword)){
+            sscanf(input_line,"%*s%*s%lf",&r_conv_hydro_to_dyn);
+	}else if(!strcmp("tf",keyword)){
+            sscanf(input_line,"%*s%*s%lf",&t_conv_hydro_to_dyn);
+	}else{
+	    add_story_line(hydro_story, input_line);
+	}
+    }
+    return s;
+}
+
+ostream& chydro::print_hydro_story(ostream& s)
+{
+    put_story_header(s, HYDRO_ID);
+
+    put_real_number(s, "  R_eff =  ", effective_radius);
+    put_real_number(s, "  M_core =  ", core_mass);
+    put_real_number(s, "  R_core =  ", core_radius);
+
+    put_real_number(s, "  mf =  ", m_conv_hydro_to_dyn);
+    put_real_number(s, "  rf =  ", r_conv_hydro_to_dyn);
+    put_real_number(s, "  tf =  ", t_conv_hydro_to_dyn);
+
+    if (hydro_story)
+        put_story_contents(s, *hydro_story);
+    put_story_footer(s, HYDRO_ID);
+    
+    return s;
+}
+
