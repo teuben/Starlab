ed7282431583f2eff811cb3cefb551138887d973
starlab <starlab>
2000-10-13 20:21:59 +0000

    Initial revision

---
+++ src/node/util/rmtree.C
@@ -0,0 +1,22 @@
+/*
+ *  rmtree.C: recursively deletes a node together with all of its offspring
+ *.............................................................................
+ *    version 1:  Nov 1994   Piet Hut
+ *.............................................................................
+ */
+#include "node.h"
+
+void rmtree(node* b,
+	    bool delete_b)	// default = true
+{
+    node* d = b->get_oldest_daughter();
+    while (d) {
+	node* tmp = d->get_younger_sister();
+	rmtree(d);
+	d = tmp;
+    }
+
+    if (delete_b) delete b;	// optionally leave node itself untouched
+}
+
+/* endof: rmtree.c */
