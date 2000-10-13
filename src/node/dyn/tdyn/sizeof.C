2ed49b25ba51695ae5e1ba93169f8636963af7fc
starlab <starlab>
2000-10-13 01:28:16 +0000

    Initial revision

---
+++ src/node/dyn/tdyn/sizeof.C
@@ -0,0 +1,18 @@
+#include "tdyn.h"
+#include "hdyn.h"
+#include "worldline.h"
+
+main()
+{
+    // Interesting information:
+
+    PRL(sizeof(node));
+    PRL(sizeof(dyn));
+    PRL(sizeof(tdyn));
+    PRL(sizeof(_dyn_));
+    PRL(sizeof(hdyn));
+    PRL(sizeof(segment));
+    PRL(sizeof(worldline));
+    PRL(sizeof(hydrobase));
+    PRL(sizeof(starbase));
+}
