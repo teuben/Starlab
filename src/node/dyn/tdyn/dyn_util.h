670bb8f0f3e6b36a6d7dda13bba2c1b0b445a9d2
starlab <starlab>
2000-10-20 05:36:34 +0000

    Initial revision

---
+++ src/node/dyn/tdyn/dyn_util.h
@@ -0,0 +1,9 @@
+
+// From dyn_util.C:
+
+void convert_relative_to_absolute(dyn*);
+void accumulate_potential_energy(dyn*, dyn*, real&, real&, dynptr&);
+void compute_energies(dyn*, dyn*, char&);
+int nearest_index(dyn* b, float, float, int, int);
+int count_nodes(dyn*);
+
