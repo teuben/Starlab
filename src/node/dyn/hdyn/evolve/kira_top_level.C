7fbbbc378749632629958fa6d65625fc3fe27c1f
starlab <starlab>
2000-11-03 21:13:35 +0000

    Initial revision

---
+++ src/node/dyn/hdyn/evolve/kira_top_level.C
@@ -0,0 +1,31 @@
+
+       //=======================================================//    _\|/_
+      //  __  _____           ___                    ___       //      /|\
+     //  /      |      ^     |   \  |         ^     |   \     //          _\|/_
+    //   \__    |     / \    |___/  |        / \    |___/    //            /|\
+   //       \   |    /___\   |  \   |       /___\   |   \   // _\|/_
+  //     ___/   |   /     \  |   \  |____  /     \  |___/  //   /|\     
+ //                                                       //            _\|/_
+//=======================================================//              /|\
+
+#include "hdyn.h"
+
+// The n_top_level functions are a convenient way of keeping track
+// of the total number of top-level nodes.  *Much* more efficient than
+// using root->n_daughters().  They should probably be made part of
+// the node class eventually...
+//
+// n_top_level is maintained via set_n_top_level in kira.C as the tree
+// changes.  Currently, get_n_top_level is used only in kira_ev.C.
+
+static int n_top_level = 0;
+
+void set_n_top_level(hdyn* b)
+{
+    n_top_level = b->n_daughters();
+}
+
+int get_n_top_level()
+{
+    return n_top_level;
+}
