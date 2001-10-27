6b61070bd7785b3bda1603031fa956d974a75147
steve <steve>
2001-10-27 01:01:50 +0000

    Added velocity scaling factor as optional arbument.

--- src/node/dyn/util/freeze.C
+++ src/node/dyn/util/freeze.C
@@ -2,31 +2,34 @@
 //// freeze:  set all velocities to zero, leaving the positions unchanged.
 ////
 //// Options:     -c    add a comment to the output snapshot [false]
+////              -f    specify freeze factor [0]
 
 //   version 1:  Dec 1992   Piet Hut
+//               Oct 2001   Steve McMIllan
 
 #include "dyn.h"
 
 #ifdef TOOLBOX
 
-local void freeze(dyn * b)
-    {
+local void freeze(dyn * b, real fac)
+{
     dyn * bi;
 
-    for (bi=b->get_oldest_daughter(); bi != NULL; bi=bi->get_younger_sister())
-	bi->scale_vel(0);
-    }
+    for_all_daughters(dyn, b, bi)
+	bi->scale_vel(fac);
+}
 
 main(int argc, char ** argv)
 {
     bool  c_flag = FALSE;
     char  *comment;
+    real fac = 0;
 
     check_help();
 
     extern char *poptarg;
     int c;
-    char* param_string = "c:";
+    char* param_string = "c:f:";
 
     while ((c = pgetopt(argc, argv, param_string)) != -1)
 	switch(c) {
@@ -34,6 +37,8 @@ main(int argc, char ** argv)
 	    case 'c': c_flag = TRUE;
 		      comment = poptarg;
 		      break;
+	    case 'f': fac = atof(poptarg);
+	    	      break;
             case '?': params_to_usage(cerr, argv[0], param_string);
 	              get_help();
                       exit(1);
@@ -41,16 +46,15 @@ main(int argc, char ** argv)
 
     dyn *b;
 
-    while (b = get_dyn(cin))
-	{
+    while (b = get_dyn(cin)) {
         if (c_flag == TRUE)
             b->log_comment(comment);
         b->log_history(argc, argv);
 
-        freeze(b);
+        freeze(b, fac);
 	put_dyn(cout, *b);	
 	rmtree(b);
-	}
+    }
 }
 
 #endif
