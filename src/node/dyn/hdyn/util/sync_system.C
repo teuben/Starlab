bec9f46cdba6a7d18046d932b63e25ac21e78d00
spz <spz>
2002-03-20 16:06:48 +0000

    *** empty log message ***

---
+++ src/node/dyn/hdyn/util/sync_system.C
@@ -0,0 +1,86 @@
+
+       //=======================================================//    _\|/_
+      //  __  _____           ___                    ___       //      /|\ ~
+     //  /      |      ^     |   \  |         ^     |   \     //          _\|/_
+    //   \__    |     / \    |___/  |        / \    |___/    //            /|\ ~
+   //       \   |    /___\   |  \   |       /___\   |   \   // _\|/_
+  //     ___/   |   /     \  |   \  |____  /     \  |___/  //   /|\ ~
+ //                                                       //            _\|/_
+//=======================================================//              /|\ ~
+
+//// sync_system:  Synchronize system to the system time.
+////               This rather dumb function postulates that the times 
+////               of all nodes are forced to be equals the system time.
+////               It solves the non-restartability ofter having run with
+////               slow KS (-K option in kira).
+////               The problem with this solution is that it randomly shifts
+////               the orbital phase of KS binaries.
+////               On the other hand, it allows to restarts kira.
+////               It is recommended to use this function sparsely.
+////
+
+//   version 1:  March 2002   Simon Portegies Zwart
+
+#include "hdyn.h"
+
+#ifdef TOOLBOX
+
+void sync_system(hdyn *b, xreal time) {
+
+    xreal ct;
+    for_all_nodes(hdyn, b, bb) {
+      ct = bb->get_time();
+      if(ct != time) {
+	cerr << "Time of particle " << bb->get_name() 
+	     << " ("<<ct<<" != "<< time <<")"
+	     << " system time." << endl;
+	bb->set_time(time);
+      }
+    }
+}
+
+main(int argc, char ** argv)
+{
+    bool c_flag = false;
+    char  *comment;
+    xreal time = 0;
+
+    check_help();
+
+    extern char *poptarg;
+    extern char *poparr[];
+    int c;
+    char* param_string = "c:t:";
+
+    while ((c = pgetopt(argc, argv, param_string)) != -1)
+	switch(c) {
+	    case 'c':	c_flag = TRUE;
+	    		comment = poptarg;
+	    		break;
+  	    case 't':	atof(poptarg);
+			break;
+            case '?':	params_to_usage(cerr, argv[0], param_string);
+	    		get_help();
+	    		exit(1);
+        }            
+
+    hdyn *b;
+
+    while (b = get_hdyn(cin)) {
+
+        if (c_flag == TRUE)
+            b->log_comment(comment);
+        b->log_history(argc, argv);
+
+	time = b->get_system_time();
+
+	cerr << "Synchronize system to t = " << time << endl;
+
+	sync_system(b, time);
+
+	put_hdyn(cout, *b);	
+	rmtree(b);
+    }
+}
+
+#endif
