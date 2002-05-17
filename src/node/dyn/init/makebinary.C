6ed64dd49416f8e59c04f8bd34faa7e6e20bae45
steve <steve>
2002-05-17 02:43:32 +0000

    Added comment.

--- src/node/dyn/init/makebinary.C
+++ src/node/dyn/init/makebinary.C
@@ -264,7 +264,7 @@ local void mkbinary(dyn* b, real lower, real upper,
 		    a_min = b->get_starbase()->conv_r_star_to_dyn(a_min);
 
 		    //binding energy = 0.5 * m_total / sma;
-		    real min_binding_energy = 10; // [kT]
+		    real min_binding_energy = 10; // [kT] -- arbitrary!
 		    real a_max = 0.5 * m_total/min_binding_energy;
 		    if(upper>a_min) {
 		      a_max = min(a_max, upper);
