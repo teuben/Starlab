8b0da77ea61232e472573fdcf11406f360b47afd
steve <steve>
2002-05-30 04:11:50 +0000

    Combined code timing options in a single header file.

---
+++ src/node/dyn/hdyn/evolve/kira_timing.h
@@ -0,0 +1,20 @@
+
+// Turn on/off various pieces of timing code.
+
+// In calculate_acc_and_jerk_for_list (kira_ev.C):  Set TIME_INTERNAL
+// to perform timing of force-evaluation functions.
+
+#define TIME_INTERNAL
+#undef  TIME_INTERNAL
+
+// In integrate_list (kira.C):  Define TIME_LIST to time various force
+// calculations.
+
+#define TIME_LIST
+#undef  TIME_LIST
+
+// In integrate_list (kira.C) and calculate_acc_and_jerk_for_list (kira_ev.C):
+// Define CPU_COUNTERS to time various parts of the integration procedure.
+
+#define CPU_COUNTERS
+//#undef  CPU_COUNTERS
