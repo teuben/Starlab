1c3e1d9ad6776bfeebeba22aeec44e0b63f9574b
steve <steve>
2002-06-01 17:23:34 +0000

    Added "-f frac" option to allow partial mergers.

--- src/node/dyn/util/merge_low.C
+++ src/node/dyn/util/merge_low.C
@@ -2,6 +2,7 @@
 //// merge:  merge all low-level nodes
 ////
 //// Options:     -c    add a comment to the output snapshot [false]
+////              -f    merge the specified fraction, chosen randomly [1]
 
 //   version 1:  September 1999		Steve McMillan
 
@@ -9,10 +10,14 @@
 
 #ifndef TOOLBOX
 
-void merge_low_level_nodes(dyn * b)	// root node
+void merge_low_level_nodes(dyn * b,	    // root node
+			   real frac)	    // default = 1
 {
     for_all_daughters(dyn, b, bb)
-	if (bb->is_parent()) {
+	if (bb->is_parent()
+	    && randinter(0,1) <= frac) {    // N.B. really random, not "first
+					    // frac" or "every 1/frac", ...
+
 	    dyn * od = bb->get_oldest_daughter();
 	    dyn * yd = od->get_younger_sister();
 
@@ -36,18 +41,21 @@ main(int argc, char ** argv)
 {
     bool  c_flag = FALSE;
     char  *comment;
+    real frac = 1;
 
     check_help();
 
     extern char *poptarg;
     int c;
-    char* param_string = "c:";
+    char* param_string = "c:f:";
 
     while ((c = pgetopt(argc, argv, param_string)) != -1)
 	switch(c) {
 
 	    case 'c': c_flag = TRUE;
 		      comment = poptarg;
+		      break;
+	    case 'f': frac = atof(poptarg);
 		      break;
             case '?': params_to_usage(cerr, argv[0], param_string);
 	              get_help();
@@ -62,7 +70,7 @@ main(int argc, char ** argv)
             b->log_comment(comment);
         b->log_history(argc, argv);
 
-        merge_low_level_nodes(b);
+        merge_low_level_nodes(b, frac);
 	put_dyn(cout, *b);	
 	delete b;
     }
