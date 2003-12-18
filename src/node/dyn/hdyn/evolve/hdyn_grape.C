b2345c9e0161b453911915f36eb3ca58c34560c1
steve <steve>
2003-12-18 20:04:30 +0000

    Replaced file formerly removed from the archive...

---
+++ src/node/dyn/hdyn/evolve/hdyn_grape.C
@@ -0,0 +1,26 @@
+
+       //=======================================================//    _\|/_
+      //  __  _____           ___                    ___       //      /|\ ~
+     //  /      |      ^     |   \  |         ^     |   \     //          _\|/_
+    //   \__    |     / \    |___/  |        / \    |___/    //            /|\ ~
+   //       \   |    /___\   |  \   |       /___\   |   \   // _\|/_
+  //     ___/   |   /     \  |   \  |____  /     \  |___/  //   /|\ ~
+ //                                                       //            _\|/_
+//=======================================================//              /|\ ~
+
+//  hdyn_grape.C:  Simple switch between GRAPE-4 and GRAPE-6 functions.
+//		   Default is to compile GRAPE-4 code, unless GRAPE-6
+//		   is specifically selected by setting the variable
+//		   STARLAB_HAS_GRAPE6 at compile time.
+//
+//    version 1:  Jul 2000   Steve McMillan
+
+#if defined(STARLAB_HAS_GRAPE6)
+
+#   include "hdyn_grape6.C"
+
+#else
+
+#   include "hdyn_grape4.C"
+
+#endif
