cd0b913a54cd780ef501f1dc49afec647a65120b
steve <steve>
2004-09-07 20:11:32 +0000

    Replaced ints.C by ints.c -- C is still more portable!

---
+++ sbin/ints.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Handy program to compare sizes of various integer types.
+
+main()
+{
+    printf("sizeof(short int) = %d\n", sizeof(short int));
+    printf("sizeof(int) = %d\n", sizeof(int));
+    printf("sizeof(long int) = %d\n", sizeof(long int));
+    printf("sizeof(long long) = %d\n", sizeof(long long));
+}
+
