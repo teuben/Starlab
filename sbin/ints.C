e391bfcef5f8cbfa527f7ac62298767dadebc14d
steve <steve>
2004-09-07 18:40:02 +0000

    Added "ints" program to print lengths of various int types.

---
+++ sbin/ints.C
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Handy program to compare sizes of various integer types.
+
+main()
+{
+    cout << "sizeof(short int) = " << sizeof(short int) << endl;
+    cout << "sizeof(int) = " << sizeof(int) << endl;
+    cout << "sizeof(long int) = " << sizeof(long int) << endl;
+    cout << "sizeof(long long) = " << sizeof(long long) << endl;
+}
+
