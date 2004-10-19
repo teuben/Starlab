51c180483e736985584932a6f25025ecb651e60b
steve <steve>
2004-10-19 23:04:40 +0000

    Added cvs versioninfo.

--- src/node/dyn/init/makecube.C
+++ src/node/dyn/init/makecube.C
@@ -99,7 +99,8 @@ main(int argc, char ** argv) {
     int c;
     char* param_string = "c:CilL:n:os:u";
 
-    while ((c = pgetopt(argc, argv, param_string)) != -1)
+    while ((c = pgetopt(argc, argv, param_string,
+		    "$Revision$", _SRC_)) != -1)
 	switch(c)
 	    {
 	    case 'c': c_flag = true;
