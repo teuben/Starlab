fa002a1ec5f8f88c6fe2f8d837ba97895013e8d1
spz <spz>
2004-11-29 22:46:23 +0000

    *** empty log message ***

--- src/node/dyn/init/makeblack_hole.C
+++ src/node/dyn/init/makeblack_hole.C
@@ -33,8 +33,7 @@ char  tmp_string[SEED_STRING_LENGTH];
 #ifdef TOOLBOX
 
 local void makeblack_hole(dyn* b, int id, real m_bh,
-			  bool set_flag,
-			  vec r_bh, vec v_bh) {
+			  bool r_flag, vec r_bh, bool v_flag, vec v_bh) {
 
     b->to_com();
 
@@ -68,13 +67,13 @@ local void makeblack_hole(dyn* b, int id, real m_bh,
     if(bh) {
         prev_mass = bh->get_mass();
 	bh->set_mass(m_bh);
-	if(set_flag) {
 	  cerr << "Set Black hole position and velocity" << endl;
-	  bh->set_pos(r_bh);
-	  bh->set_vel(v_bh);
-	}
-	else
-	  bh->set_vel(bh->get_vel() * sqrt(prev_mass/m_bh));
+	  if(r_flag) 
+	    bh->set_pos(r_bh);
+	  if(v_flag) 
+	    bh->set_vel(v_bh);
+	  else
+	    bh->set_vel(bh->get_vel() * sqrt(prev_mass/m_bh));
     }
     else 
 	err_exit("makeblack_hole: selected star not found");
@@ -94,41 +93,64 @@ local void makeblack_hole(dyn* b, int id, real m_bh,
     cerr << "Black hole mass is " << m_bh << endl;
 }
 
+local inline real mass(dyn *b, real r) 
+{
+
+  real m_sum = 0;
+  for_all_leaves(dyn, b, bi) {
+    if(abs(bi->get_pos())<=r)
+      m_sum += bi->get_mass();
+  }
+  return m_sum;
+}
+
+local inline real vcirc2(dyn *b, real r)	// circular orbit speed:
+						// recall vc^2 = r d(phi)/dr
+{
+    if (r > 0)
+	return mass(b, r)/r;
+    else
+	return 0;
+}
+
 int main(int argc, char ** argv) {
+
+    bool  c_flag = FALSE;
+    char  *comment;
 
     real m_bh = 0;
     int id = -1;    // most central particle is selected
+    bool r_flag = false;
     vec r_bh = 0;
+    bool v_flag = false;
     vec v_bh = 0;
-    bool set_flag = false;
-
 
     check_help();
 
     extern char *poptarg;
-    extern char *poparr[3];
+    extern char *poparr[];
 
     int c;
-    char* param_string = "fi:M:r:v:";
+    char* param_string = "c:i:M:r:::v:::";
 
     while ((c = pgetopt(argc, argv, param_string,
 		    "$Revision$", _SRC_)) != -1)
 	switch(c) {
+	    case 'c':	c_flag = TRUE;
+	    		comment = poptarg;
+	    		break;
 	    case 'M': m_bh = atof(poptarg);
 		      break;
 	    case 'r':	r_bh = vec(atof(poparr[0]),
 				   atof(poparr[1]),
 				   atof(poparr[2]));
-	                set_flag = true;
+	                r_flag = true;
 	    		break;
 	    case 'v':	v_bh = vec(atof(poparr[0]),
 				   atof(poparr[1]),
 				   atof(poparr[2]));
-	                set_flag = true;
+	                v_flag = true;
 	    		break;
-	    case 'f': set_flag = true;
-	    		break;
-
 	    case 'i': id = atoi(poptarg);
 		      break;
             case '?': params_to_usage(cerr, argv[0], param_string);
@@ -136,32 +158,44 @@ int main(int argc, char ** argv) {
 	}
 
     dyn* b;
-    b = get_dyn();
-    b->log_history(argc, argv);
+    while (b = get_dyn()) {
 
-    if (id>b->n_leaves())
-      err_exit("selected id exceeds particle number");
+      if (c_flag == TRUE)
+	b->log_comment(comment);
+      b->log_history(argc, argv);
 
-    makeblack_hole(b, id, m_bh, set_flag, r_bh, v_bh);
+      // Check if we have to reinterpret r and v in light of possible
+      // external fields and physical parameters.
 
-    real initial_mass = getrq(b->get_log_story(), "initial_mass");
+      //    real mass, length, time;
+      //    bool phys = get_physical_scales(b, mass, length, time);
 
-    if (initial_mass > -VERY_LARGE_NUMBER)
+      if (id>b->n_leaves())
+	err_exit("selected id exceeds particle number");
+
+      if(v_flag)
+	v_bh *= sqrt(vcirc2(b, abs(r_bh)));
+
+      makeblack_hole(b, id, m_bh, r_flag, r_bh, v_flag, v_bh);
+
+      real initial_mass = getrq(b->get_log_story(), "initial_mass");
+
+      if (initial_mass > -VERY_LARGE_NUMBER)
         putrq(b->get_log_story(), "initial_mass", b->get_mass(),
 	      HIGH_PRECISION);
 
-    real m_sum = b->get_mass();
-    real old_mtot = b->get_starbase()->conv_m_dyn_to_star(1);
-    if(old_mtot!=m_sum) {
+      real m_sum = b->get_mass();
+      real old_mtot = b->get_starbase()->conv_m_dyn_to_star(1);
+      if(old_mtot!=m_sum) {
 	real old_r_vir= b->get_starbase()->conv_r_star_to_dyn(1);
 	real old_t_vir= b->get_starbase()->conv_t_star_to_dyn(1);
 	b->get_starbase()->set_stellar_evolution_scaling(m_sum,
 							 old_r_vir,
 							 old_t_vir);
-    }
+      }
 
-    put_dyn(b);
-    rmtree(b);
-    return 0;
+      put_dyn(b);
+      rmtree(b);
+    }
 }
 #endif
