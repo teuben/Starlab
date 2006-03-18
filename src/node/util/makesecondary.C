8d7779ab80c3596d8f556e9397bff0ea9d8f34fe
steve <steve>
2006-03-18 21:56:59 +0000

    Merged node and dyn versions; corrected bug in determination of total mass in cases where substructure already exists.

--- src/node/util/makesecondary.C
+++ src/node/util/makesecondary.C
@@ -10,17 +10,26 @@
 
 //// Create binary secondary components for randomly selected stars
 //// in the input snapshot, placing the results in a binary tree.
-//// Only secondary masses are set here; orbital parameters are set
-//// by dyn::makebinary.  No attempt is made to maintain any existing
-//// mass or energy scaling.  Use scale after this function is called,
-//// but before makebinary, if it is desired to specify energies in
-///// kT units.
+//// Note that only top-level nodes are affected, and the binary
+//// fraction is determined relative to the number of top-level nodes.
+//// This function may be called several times to create several
+//// classes of binaries in a single system.
 ////
-//// Usage: makesecondary [OPTIONS]
+//// Only secondary masses are set here; orbital parameters are set
+//// by dyn::makebinary.
+////
+//// No attempt is made to maintain any existing mass or energy scaling.
+//// Use scale after this function is called, but before makebinary, if
+//// it is desired to specify energies in kT units.
+////
+//// Usage: makesecondary [OPTIONS] < input > output
 ////
 //// Options:
-////          -f    specify binary fraction [0.1] for stars with
-////                mass >= value specified with -m
+////          -C    force output in 'col' format (dyn version only) [no]
+////                (note: loses binary data)
+////          -f    specify binary fraction for top-level stars having
+////                masses greater than or equal to the value specified
+////                with the "-m" option [0.1]
 ////          -i    use (a,b) as component indices [false]
 ////          -I    don't limit masses to primary mass range [false]
 ////          -l    specify lower limit on mass ratio or secondary mass [0]
@@ -44,14 +53,20 @@
 //   Steve McMillan, July 1996
 //   Simon Portegies Zwart, Tokyo, December 1997
 
-#include "node.h"
+#ifndef DYN
+#   include "node.h"
+#   define NODE node
+#else
+#   include "dyn.h"
+#   define NODE dyn
+#endif
 
 #define  SEED_STRING_LENGTH  256
 char  tmp_string[SEED_STRING_LENGTH];
 
 #ifdef TOOLBOX
 
-local void name_from_components(node *od, node *yd)
+local void name_from_components(NODE *od, NODE *yd)
 {
     char name1[256], name[256];
     strcpy(name1, od->format_label());
@@ -71,12 +86,12 @@ local void name_from_components(node *od, node *yd)
 //		  numeric names are now the default, with (a,b) the
 //		  alternative.  (Steve, 5/01)
 
-local real add_secondary(node* original, real mass_ratio,
+local real add_secondary(NODE* original, real mass_ratio,
 			 bool force_index, int &nmax,
 			 bool split)
 {
-    node* primary = new node;
-    node* secondary = new node;
+    NODE* primary = new NODE;
+    NODE* secondary = new NODE;
 
     // Add new links.
 
@@ -156,7 +171,7 @@ local real add_secondary(node* original, real mass_ratio,
 local void warning(real mmin, real lower_limit) {
 
   cerr << "WARNING:" << endl;
-  cerr << "       local void makesecondary(node*, real, real, bool)" << endl;
+  cerr << "       makesecondary:" << endl;
   cerr << "       actual minimum mass " << mmin
        << " is larger than lower limit " << lower_limit << ";" << endl;
   cerr << "       continuing execution with true minimum mass" << endl;
@@ -171,26 +186,26 @@ local real random_mass_ratio(const real qmin,
   return randinter(qmin, qmax);
 }
 
-local void name_recursive(node *b)
+local void name_recursive(NODE *b)
 {
     // Make a standard CM name, making sure that we build names
     // from the bottom up.
 
-    node *od = b->get_oldest_daughter();
+    NODE *od = b->get_oldest_daughter();
     if (od) {
-	node *yd = od->get_younger_sister();
+	NODE *yd = od->get_younger_sister();
 	name_recursive(od);
 	name_recursive(yd);
 	name_from_components(od, yd);
     }
 }
 
-local int check_indices(node *b)
+local int check_indices(NODE *b)
 {
     int nmax = 0;
     bool all_ok = true;
 
-    for_all_leaves(node, b, bb) {
+    for_all_leaves(NODE, b, bb) {
 
 	bb->set_name(NULL);
 
@@ -204,7 +219,7 @@ local int check_indices(node *b)
 
 	cerr << "    makesecondary: assigning numeric indices to nodes" << endl;
 
-	for_all_leaves(node, b, bb) {
+	for_all_leaves(NODE, b, bb) {
 	    if (bb->get_index() <= 0)
 		bb->set_index(++nmax);
 	}
@@ -212,18 +227,18 @@ local int check_indices(node *b)
 
     // Redetermine all center-of-mass names.
 
-    for_all_nodes(node, b, bb)
+    for_all_nodes(NODE, b, bb)
 	if (bb!= b && bb->is_parent())
 	    name_recursive(bb);
 
     return nmax;
 }
 
-local void makesecondary(node* b, real binary_fraction,
-		       real min_mprim, real max_mprim,
-		       real lower_limit, real upper_limit, 
-		       bool force_index, bool q_flag, bool ignore_limits,
-		       bool split)
+local void makesecondary(NODE* b, real binary_fraction,
+			 real min_mprim, real max_mprim,
+			 real lower_limit, real upper_limit, 
+			 bool force_index, bool q_flag, bool ignore_limits,
+			 bool split)
 {
     PRI(4); PRL(binary_fraction);
     PRI(4); PRL(min_mprim);
@@ -253,18 +268,16 @@ local void makesecondary(node* b, real binary_fraction,
     // Assume that the probability of a star being the primary of
     // a binary is independent of mass.
 
-    real sum = 0;
-    b->set_mass(0);
-
     real m_prim, q;
     real m_primaries=0, m_secondaries=0, m_total=0;
+    real sum = 0;
 
     if (q_flag) {
 
         // Choose the secondary mass ratio uniformly on
 	// [lower_limit, upper_limit].
 
-	for_all_daughters(node, b, bi) {
+	for_all_daughters(NODE, b, bi) {	// top level nodes only
 	    if(!bi->is_parent()) {
 
 		m_prim = bi->get_mass();
@@ -277,7 +290,8 @@ local void makesecondary(node* b, real binary_fraction,
 						// 9/04
 			q = random_mass_ratio(lower_limit, upper_limit);
 			
-			m_secondaries += add_secondary(bi, q, force_index, nmax, split);
+			m_secondaries += add_secondary(bi, q, force_index,
+						       nmax, split);
 		    }
 		}
 	    }
@@ -298,7 +312,7 @@ local void makesecondary(node* b, real binary_fraction,
 
 	} else {
 
-	    for_all_daughters(node, b, bi) {
+	    for_all_daughters(NODE, b, bi) {
 		if (bi->is_leaf()) {
 		    mmin = Starlab::min(mmin, bi->get_mass());
 		    mmax = Starlab::max(mmax, bi->get_mass());
@@ -322,7 +336,7 @@ local void makesecondary(node* b, real binary_fraction,
 	    
 	// Note that the following loop is essentially the same code as above.
 
-	for_all_daughters(node, b, bi) {
+	for_all_daughters(NODE, b, bi) {	// top level nodes only
 	    if(!bi->is_parent()) {
 
 		m_prim = bi->get_mass();
@@ -336,22 +350,23 @@ local void makesecondary(node* b, real binary_fraction,
 			qmax = Starlab::min(mmax,
 					    bi->get_mass())/bi->get_mass();
 			q = random_mass_ratio(qmin, qmax);
-			m_secondaries += add_secondary(bi, q, force_index, nmax, split);
+			m_secondaries += add_secondary(bi, q, force_index,
+						       nmax, split);
 		    }
 		}
 	    }
 	}
     }
 
-    real m_tot = m_primaries + m_secondaries;
-    if(split) {
-	m_tot = m_primaries;
-	m_primaries -= m_secondaries;
-    }
+    if(split) m_primaries -= m_secondaries;
+
+    // Recompute the total system mass.
+
+    real m_tot = get_mass(b);
     b->set_mass(m_tot);	
     real old_mtot = b->get_starbase()->conv_m_dyn_to_star(1);
 
-    if(old_mtot>0 && old_mtot<m_tot) {
+    if(old_mtot > 0 && old_mtot < m_tot) {
 	real old_r_vir= b->get_starbase()->conv_r_star_to_dyn(1);
 	real old_t_vir= b->get_starbase()->conv_t_star_to_dyn(1);
 	b->get_starbase()->set_stellar_evolution_scaling(m_tot,
@@ -377,6 +392,9 @@ local void makesecondary(node* b, real binary_fraction,
 
 int main(int argc, char ** argv)
 {
+#ifdef DYN
+    bool C_flag = false;
+#endif
 
     bool split = false;
     bool ignore_limits = false;
@@ -395,12 +413,21 @@ int main(int argc, char ** argv)
 
     extern char *poptarg;
     int c;
+
+#ifndef DYN
     char* param_string = "qf:M:m:il:nu:Ss:I";
+#else
+    char* param_string = "Cqf:M:m:il:nu:Ss:I";
+#endif
 
     while ((c = pgetopt(argc, argv, param_string,
 		    "$Revision$", _SRC_)) != -1)
 	switch(c) {
 
+#ifdef DYN
+	    case 'C': C_flag = true;
+		      break;
+#endif
 	    case 'f': binary_fraction = atof(poptarg);
 		      break;
 	    case 'i': force_index = true;
@@ -452,8 +479,12 @@ int main(int argc, char ** argv)
     if (max_mprim < min_mprim)
         err_exit("Invalid upper and lower primary mass selections");
 
-    node *b;
+    NODE *b;
+#ifndef DYN
     b = get_node();
+#else
+    b = get_dyn();
+#endif
 
     b->log_history(argc, argv);
 
@@ -466,7 +497,14 @@ int main(int argc, char ** argv)
 		lower_limit, upper_limit, force_index, q_flag,
 		ignore_limits, split);
 
+#ifdef DYN
+    b->set_col_output(false);
+    if (C_flag) b->set_col_output(true);
+    put_dyn(b);
+#else
     put_node(b);
+#endif
+
     rmtree(b);
     return 0;
 }
