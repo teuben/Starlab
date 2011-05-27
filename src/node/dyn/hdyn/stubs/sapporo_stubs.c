08746e3a7997c8ea8631c29bc82fb45f3f46e331
steve <steve>
2011-05-27 21:33:15 +0000

    Stubs file to accommodate changes in Sapporo v.1.6b.

---
+++ src/node/dyn/hdyn/stubs/sapporo_stubs.c
@@ -0,0 +1,25 @@
+
+       //=======================================================//    _\|/_
+      //  __  _____           ___                    ___       //      /|\ ~
+     //  /      |      ^     |   \  |         ^     |   \     //          _\|/_
+    //   \__    |     / \    |___/  |        / \    |___/    //            /|\ ~
+   //       \   |    /___\   |  \   |       /___\   |   \   // _\|/_
+  //     ___/   |   /     \  |   \  |____  /     \  |___/  //   /|\ ~
+ //                                                       //            _\|/_
+//=======================================================//              /|\ ~
+
+#include "stdio.h"
+
+// sapporo_stubs.C: Extra stubs based on the sapporo1.6 library.
+//
+// Steve McMillan, May 2011
+
+
+int g6_open_special(int ngpu, int *list, int Nmax) {
+    fprintf(stderr, "*** error: g6_open_special not implemented ***\n");
+    return 0;
+};
+int g6_open_special2(int ngpu, int *list, int Nmax) {
+    fprintf(stderr, "*** error: g6_open_specials not implemented ***\n");
+    return 0;
+};
